681f62a948d88ecea55057f2c5734c4a84cb5100
Wojciech Pruszak <info@wpruszak.com>
2016-09-25 08:25:52 +0200

    Hotel bundle, reformatted code.

--- app/autoload.php
+++ app/autoload.php
@@ -1,13 +1,13 @@
 <?php
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-use Composer\Autoload\ClassLoader;
+    use Doctrine\Common\Annotations\AnnotationRegistry;
+    use Composer\Autoload\ClassLoader;
 
-/**
- * @var ClassLoader $loader
- */
-$loader = require __DIR__.'/../vendor/autoload.php';
+    /**
+     * @var ClassLoader $loader
+     */
+    $loader = require __DIR__ . '/../vendor/autoload.php';
 
-AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+    AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
-return $loader;
+    return $loader;
