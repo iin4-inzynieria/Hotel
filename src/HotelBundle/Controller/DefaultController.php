1efca38646a901e71a60e63acf6910f91532a9e8
Wojciech Pruszak <info@wpruszak.com>
2016-10-23 16:37:27 +0200

    Moving filtering code to ReservationController.

--- src/HotelBundle/Controller/DefaultController.php
+++ src/HotelBundle/Controller/DefaultController.php
@@ -25,26 +25,4 @@ class DefaultController extends Controller {
             'form' => $stepOneForm->createView()
         ]);
     }
-
-    /**
-     * Renders list room view. Lists only rooms that are matching given
-     * criteria (eg. only available rooms).
-     *
-     * @param Request $request
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function filterAction(Request $request) {
-
-        $data = $request->request->get('step_one');
-
-        $arrival = new \DateTime($data['arrival']);
-        $departure = new \DateTime($data['departure']);
-
-        $entities = $this->getDoctrine()->getRepository('CoreBundle:Room')->getAvailableByDatePeriod($arrival, $departure);
-
-        return $this->render('HotelBundle:Index:list_available_rooms.html.twig', array(
-            'rooms' => $entities
-        ));
-    }
 }
