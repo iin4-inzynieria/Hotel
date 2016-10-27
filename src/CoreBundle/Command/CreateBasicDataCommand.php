995765813ea609d29998c8bcdf3fcdf44ab62ca7
Wojciech Pruszak <setzo94@gmail.com>
2016-10-27 13:07:10 +0200

    Command fix - added new fields.

--- src/CoreBundle/Command/CreateBasicDataCommand.php
+++ src/CoreBundle/Command/CreateBasicDataCommand.php
@@ -49,12 +49,21 @@ class CreateBasicDataCommand extends ContainerAwareCommand
     {
         $roomOne = new Room();
         $roomOne->setTitle('Pokój pierwszy');
+        $roomOne->setDescription('pokój 1 desc');
+        $roomOne->setImageLink('http://img.shmbk.pl/rimgsph/2036_e6990baa-cd5f-48a1-9455-d293fb339b57_max_900_1200_-pokoj-dziecka-styl-nowoczesny.jpg');
+        $roomOne->setRoomType(Room::ROOM_TYPE_DEFAULT);
 
         $roomTwo = new Room();
         $roomTwo->setTitle('Pokój drugi');
+        $roomTwo->setDescription('pokój 2 desc');
+        $roomTwo->setImageLink('http://img.shmbk.pl/rimgsph/2729_f31d2ad9-5cb6-45f8-ac8c-ffded6cab00b_max_900_1200_-pokoj-dziecka-styl-glamour.jpg');
+        $roomTwo->setRoomType(Room::ROOM_TYPE_DEFAULT);
 
         $roomThree = new Room();
         $roomThree->setTitle('Pokój trzeci');
+        $roomThree->setDescription('pokój 3 desc');
+        $roomThree->setImageLink('http://www.brw.com.pl/gfx/brw/_thumbs/pl/brwaranzacje/396/s31_pokoj-mlodziezowy_dsu_1hd,klWZfqWjaWRC29OS1G5eng.jpg');
+        $roomThree->setRoomType(Room::ROOM_TYPE_APARTMENT);
 
         return [$roomOne, $roomTwo, $roomThree];
     }
